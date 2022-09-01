a7889b450c390d5acd3437318dcf14ac208158ea
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-01 01:13:56 -0700

    init

--- projects/hardhat-lottery/contracts/raffle.sol
+++ projects/hardhat-lottery/contracts/raffle.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.9;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
 
-contract raffle is VRFConsumerBaseV2 {
+contract raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
   /* State Variables */
   uint256 private immutable i_entranceFee;
   address payable[] private s_players;
@@ -16,11 +17,16 @@ contract raffle is VRFConsumerBaseV2 {
   uint32 private constant NUM_WORDS = 2;
   VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
 
+  // Lottery Variables
+  address private s_recentWinner;
+
   /* Events */
   event RaffleEnter(address indexed player);
   event RequestedRaffleWinner(uint256 indexed requestId);
+  event WinnerPicked(address indexed winner);
 
   error raffle__NotEnoughETHEntered();
+  error raffle__TransferFailed();
 
   constructor(
     address VRFCoordinatorV2,
@@ -44,6 +50,18 @@ contract raffle is VRFConsumerBaseV2 {
     emit RaffleEnter(msg.sender);
   }
 
+  /**
+   * @dev this is the function chainlink keepers call
+   * they look for updkeepneeded to return true.
+   * following should be ture in order to return ture:
+   * 1. our time interval should have passed
+   * 2. lottery should have at least 1 player and have some eth
+   * 3. our subscription is funded with ETH.
+   */
+  function checkUpKeep(
+    bytes calldata /*checkData*/
+  ) external override {}
+
   // external cheaper than public
   function pickRandomWinner() external {
     // request random number and once we get it do something with it
@@ -57,15 +75,31 @@ contract raffle is VRFConsumerBaseV2 {
     emit RequestedRaffleWinner(requestId);
   }
 
-  function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {}
+  function fulfillRandomWords(
+    uint256,
+    /*requestId*/
+    uint256[] memory randomWords
+  ) internal override {
+    uint256 indexOfWinner = randomWords[0] % s_players.length;
+    address payable recentWinner = s_players[indexOfWinner];
+    (bool success, ) = recentWinner.call{value: address(this).balance}("");
+    if (!success) {
+      revert raffle__TransferFailed();
+    }
+    emit WinnerPicked(recentWinner);
+  }
 
-  /* view / pure functinos */
+  /* view / pure functinos
   function getEntranceFee() public view returns (uint256) {
     return i_entranceFee;
   }
 
   function getPlayer(uint256 index) public view returns (address) {
     return s_players[index];
+  }
+
+  function getRecentWinner() public view returns (address) {
+    return s_recentWinner;
   }
 }
 
