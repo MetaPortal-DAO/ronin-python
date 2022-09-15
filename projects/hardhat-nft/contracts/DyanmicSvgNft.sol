84033f6ff21e3c5f34b4064a77bafc63c6d9b7a9
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-15 19:23:04 +0200

    uri encoding

--- projects/hardhat-nft/contracts/DyanmicSvgNft.sol
+++ projects/hardhat-nft/contracts/DyanmicSvgNft.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721, Ownable {
@@ -14,11 +15,18 @@ contract DynamicSvgNft is ERC721, Ownable {
   string private i_lowImageURI;
   string private i_highImageURI;
   string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+  AggregatorV3Interface internal immutable i_priceFeed;
+  mapping(uint256 => int256) public s_tokenIdToHighValue;
 
-  constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSM") {
+  constructor(
+    address priceFeedAddress,
+    string memory lowSvg,
+    string memory highSvg
+  ) ERC721("Dynamic SVG NFT", "DSM") {
     s_tokenCounter = 0;
-    i_lowImageURI = "";
-    i_highImageURI = "";
+    i_lowImageURI = svgToImageURI(lowSvg);
+    i_highImageURI = svgToImageURI(highSvg);
+    i_priceFeed = AggregatorV3Interface(priceFeedAddress);
   }
 
   function svgToImageURI(string memory svg) public pure returns (string memory) {
@@ -26,7 +34,8 @@ contract DynamicSvgNft is ERC721, Ownable {
     return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
   }
 
-  function mintNft() public {
+  function mintNft(int256 highValue) public {
+    s_tokenIdToHighValue[s_tokenCounter] = highValue;
     _safeMint(msg.sender, s_tokenCounter);
     s_tokenCounter++;
   }
@@ -39,11 +48,15 @@ contract DynamicSvgNft is ERC721, Ownable {
   // need it to return base64 of metadata json
   function tokenURI(uint256 tokenId) public view override returns (string memory) {
     require(_exists(tokenId), "URI Query for nonexistent token");
-    string memory imageURI = "hi";
 
     //data:image/svg+xml;base64 prefix for svg images
     //data:application/json;base64 prefix for json
 
+    (, int256 price, , , ) = i_priceFeed.latestRoundData();
+    string memory imageURI = i_lowImageURI;
+    if (price >= s_tokenIdToHighValue[tokenId]) {
+      imageURI = i_highImageURI;
+    }
     // creating json string, encoding in bytes->base64->append inital part
     return
       string(
