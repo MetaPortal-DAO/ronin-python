643ff646c93be5ba9de6bd735713bcb29266bf73
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-24 12:12:35 +0200

    connector

--- connector.py
+++ connector.py
@@ -5,12 +5,15 @@ import pandas as pd
 import os
 load_dotenv()
 
-def connect_mongo(address):
-    username = os.getenv("MONGO_USERNAME")
-    password = os.getenv("MONGO_PASSWORD")
+username = os.getenv("MONGO_USERNAME")
+password = os.getenv("MONGO_PASSWORD")
 
-    cluster = pymongo.MongoClient(f"mongodb+srv://${username}:${password}@cluster0.2lmaniy.mongodb.net/?retryWrites=true&w=majority")
-    db = cluster["ronin-indexer"]
+cluster = pymongo.MongoClient(f"mongodb+srv://${username}:${password}@cluster0.2lmaniy.mongodb.net/?retryWrites=true&w=majority")
+db = cluster["ronin-indexer"]
 
-    collection = db[address]
-    return collection 
+collection = db["0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"]
+
+results = collection.find({"from": "7543d33a8dcb325a58921e237d49ff2829b50339"})
+
+for result in results:
+    print(result["_id"])
--- main.py
+++
@@ -1,4 +0,0 @@
-from connector import connect_mongo
-
-weth_address = "0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"
-slp_address = "0xa8754b9fa15fc18bb59458815510e40a12cd2014"
