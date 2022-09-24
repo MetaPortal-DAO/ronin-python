e0837711d177011327106c43967c08664dec0744
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-24 21:52:41 +0200

    init other files

---
+++ whale_timezone.py
@@ -0,0 +1,2 @@
+# for guessing which timezones and regions whales are in by what time their tx's are based 
+# would be great to get an approx as well as some confidence interval
---
+++ whales_time_analysis.py
@@ -0,0 +1,6 @@
+# for analyzing when a whale becomes a whale 
+# are there any patterns to when they start spending big bucks? 
+
+# plot deposits into treasury over time 
+# then if there is a sudden increase, see tx around that time 
+# see if any events happened around that time as well
