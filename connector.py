66f50296176a0968d7574486c7f98155615c310e
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-24 16:01:56 +0200

    why keep on getting emptpy df DANIELLL

--- connector.py
+++ connector.py
@@ -6,6 +6,7 @@ import os
 load_dotenv()
 from dateutil import rrule
 from datetime import datetime, timedelta
+import time
 
 username = os.getenv("MONGO_USERNAME")
 password = os.getenv("MONGO_PASSWORD")
@@ -20,13 +21,14 @@ collection = db["0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"]
 
 # unix timestamp to mongodb months
 for dt in rrule.rrule(rrule.MONTHLY, dtstart=now, until=three_months_ago):
-    print(dt)
+    unixtime = time.mktime(dt.timetuple())
+    unixtime1 = time.mktime((dt+timedelta(days=30)).timetuple())
     df = pd.DataFrame(collection.aggregate([
             {
                 "$redact": {
                     "$cond": [
                         # put dates here for each month 
-                        { "$gt": [ f"{dt}", f"{dt + timedelta(days=30)}" ] },
+                        { "$gt": [ f"{unixtime}", f"{unixtime1}" ] },
                         "$$KEEP",
                         "$$PRUNE"
                     ]
