da8aa385360c02a52634d19f3954bd2089669521
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-24 21:42:17 +0200

    cleanup

--- connector.py
+++ connector.py
@@ -8,13 +8,11 @@ from dateutil import rrule
 from datetime import datetime, timedelta
 import time
 
-first_time = False
-
 username = os.getenv("MONGO_USERNAME")
 password = os.getenv("MONGO_PASSWORD")
 
 now = datetime.utcnow()
-one_month_ago = now - timedelta(days=15)
+one_month_ago = now - timedelta(days=30)
 timeframe = datetime.utcnow()
 
 cluster = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.2lmaniy.mongodb.net/?retryWrites=true&w=majority")
@@ -22,16 +20,21 @@ db = cluster["ronin-indexer"]
 
 collection = db["0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"]
 
-dflist = []
-
-# any better ways to iterate over months?
-if (first_time):
-    timeframe = now - timedelta(days=120)
-else: 
-    timeframe = one_month_ago
+def query_mongo(first_time):
+    dflist = []
+    # any better ways to iterate over months?
+    if (first_time):
+        timeframe = now - timedelta(days=120)
+    else: 
+        timeframe = one_month_ago
+        
+    for dt in rrule.rrule(rrule.MONTHLY, dtstart=timeframe, until=now):
+        df = pd.DataFrame(collection.find({'ts':{'$gte':timeframe, '$lt':now}}))
+        if first_time == True: 
+            dflist.append(dflist)
+        else: 
+            return df
     
-for dt in rrule.rrule(rrule.MONTHLY, dtstart=timeframe, until=now):
-    df = pd.DataFrame(collection.find({'ts':{'$gte':timeframe, '$lt':now}}))
-    print(df)
-    dflist.append(dflist)
+    return dflist
+
 
