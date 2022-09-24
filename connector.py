ad2094adf4a80fe76b0209dcafc21378c86b5f0a
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-24 18:07:51 +0200

    .

--- connector.py
+++ connector.py
@@ -8,11 +8,14 @@ from dateutil import rrule
 from datetime import datetime, timedelta
 import time
 
+first_time = True
+
 username = os.getenv("MONGO_USERNAME")
 password = os.getenv("MONGO_PASSWORD")
 
 now = datetime.now()
-three_months_ago = now + timedelta(days=90)
+one_month_ago = now - timedelta(days=30)
+timeframe = datetime.now()
 
 cluster = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.2lmaniy.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["ronin-indexer"]
@@ -22,7 +25,12 @@ collection = db["0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"]
 dflist = []
 
 # any better ways to iterate over months?
-for dt in rrule.rrule(rrule.MONTHLY, dtstart=now, until=three_months_ago):
+if (first_time):
+    timeframe = now - timedelta(days=120)
+else: 
+    timeframe = one_month_ago
+    
+for dt in rrule.rrule(rrule.MONTHLY, dtstart=now, until=timeframe):
     unixtime = time.mktime(dt.timetuple())
     unixtime1 = time.mktime((dt+timedelta(days=30)).timetuple())
     df = pd.DataFrame(collection.aggregate([
