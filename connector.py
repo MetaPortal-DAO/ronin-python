d34f874204693dab2755b0e60c9bcd0256172ef6
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-25 16:16:08 +0200

    caching but only for past month - is this even useful

--- connector.py
+++ connector.py
@@ -7,32 +7,56 @@ load_dotenv()
 from dateutil import rrule
 from datetime import datetime, timedelta
 import time
+import pathlib
 
 username = os.getenv("MONGO_USERNAME")
 password = os.getenv("MONGO_PASSWORD")
+WETH_STRING="0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"
 
 now = datetime.utcnow()
-one_month_ago = now - timedelta(days=30)
+
+# if we do 30 its wayyy to slow
+one_month_ago = now - timedelta(days=13)
 timeframe = datetime.utcnow()
 
 cluster = pymongo.MongoClient(f"mongodb+srv://{username}:{password}@cluster0.2lmaniy.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["ronin-indexer"]
 
-collection = db["0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"]
+collection = db[WETH_STRING]
 
+# do we need first time as an input here?
+# is it okay to just cache the past month? 
 def query_mongo(first_time):
     dflist = []
-    # any better ways to iterate over months?
+    CACHE_DIR = str(pathlib.Path().resolve()) + '/cache'
+    CACHE_FILE = CACHE_DIR + "/{}.csv".format(WETH_STRING)
+    
     if (first_time):
         timeframe = now - timedelta(days=120)
+        if not os.path.isdir(CACHE_DIR):
+            os.makedirs(CACHE_DIR)
     else: 
         timeframe = one_month_ago
-        
-    for dt in rrule.rrule(rrule.MONTHLY, dtstart=timeframe, until=now):
-        df = pd.DataFrame(collection.find({'ts':{'$gte':timeframe, '$lt':now}}))
-        if first_time == True: 
-            dflist.append(dflist)
-        else: 
-            return df
+        if not os.path.isdir(CACHE_DIR):
+            os.makedirs(CACHE_DIR)
+            
+    do = True
+
+    if os.path.isfile(CACHE_FILE):
+        # if it hasn't been 30 days
+        if (time.time() - os.path.getmtime(CACHE_FILE)  < 24*60*60*30):
+            do = False
+
+    if do == True:
+    
+        for dt in rrule.rrule(rrule.MONTHLY, dtstart=timeframe, until=now):
+            one_month_later = dt + timedelta(days=30)
+            # depending on what num we use for one_month_ago this will go in the future which is fine
+            df = pd.DataFrame(collection.find({'ts':{'$gte':dt, '$lt':(dt+timedelta(days=30))}}))
+            df.to_csv(CACHE_FILE, index=None)
+            if first_time == True: 
+                dflist.append(dflist)
     
     return dflist
+
+df = query_mongo(False)
--- whales.py
+++ whales.py
@@ -4,4 +4,5 @@ df = query_mongo(False)
 df.value = df.value.apply(int, base=16)
 
 # get WETH transfers to the treasury 
-top_whales_df = df.iloc[df.to == "ronin:a99cacd1427f493a95b585a5c7989a08c86a616b"].sort_values(ascending=False).iloc[0:100]
+top_whales_df = df.iloc[df.to == "a99cacd1427f493a95b585a5c7989a08c86a616b"].sort_values(ascending=False).iloc[0:100]
+print(top_whales_df)
