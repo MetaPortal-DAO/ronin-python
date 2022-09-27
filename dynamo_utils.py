0bfb93e63f814960fc98029da4312c1ef471390e
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-27 20:09:03 +0300

    dict keys

--- dynamo_utils.py
+++ dynamo_utils.py
@@ -4,8 +4,8 @@ import boto3
 import os
 import json
 
-ALLOWED_TABLES = {"weth":"0xa8754b9fa15fc18bb59458815510e40a12cd2014",
-                  "axs":"0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"}
+ALLOWED_TABLES = {"SLP":"0xa8754b9fa15fc18bb59458815510e40a12cd2014",
+                  "WETH":"0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"}
 
 def set_default_session(table):
     if table not in ALLOWED_TABLES.keys():
