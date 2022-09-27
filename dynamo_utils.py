30725cdb480e5bf78ad8433aad05ec7f349ec413
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2022-09-27 20:08:23 +0300

    dynamo python connect works

--- dynamo_utils.py
+++ dynamo_utils.py
@@ -4,16 +4,21 @@ import boto3
 import os
 import json
 
-ALLOWED_TABLES = []
+ALLOWED_TABLES = {"weth":"0xa8754b9fa15fc18bb59458815510e40a12cd2014",
+                  "axs":"0xc99a6a985ed2cac1ef41640596c5a5f9f4e19ef5"}
 
 def set_default_session(table):
-    if table not in ALLOWED_TABLES:
+    if table not in ALLOWED_TABLES.keys():
         raise Exception(ValueError("table not among supported erc20s"))
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb',
+                          aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+                          aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+                          region_name="us-east-1")
+    print(dynamodb)
     try: 
         table = dynamodb.Table(table)
     except:
         raise Exception(KeyError("table does not exist in dynamo"))
     return table
 
-
+set_default_session("weth")
